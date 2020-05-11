4b04ab960841f924f91cce4b5aa3028e08f2232a
Sarath <sarathjnair@gmail.com>
2020-05-11 02:34:42 +0200

    [] Split the method into small piece.

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -51,6 +51,33 @@ int calculator(int a, int b, int operationNumber) {
 }
 
 
+int getArithmeticEquation() {
+	int operationNumber;
+	Logger logger;
+	logger.logDataWithNewLine("Enter the Operation number need to perform");
+	logger.logDataWithNewLine("1. Addition");
+	logger.logDataWithNewLine("2. Substraction");
+	logger.logDataWithNewLine("3. Multiply");
+	logger.logDataWithNewLine("4. Division");
+	cin >> operationNumber;
+	return operationNumber;
+}
+
+bool doYouWantToProcessAgain() {
+	Logger logger;
+	int userInput = 0;
+	logger.logDataWithNewLine("If you want to do the calculation again press 1 otherwise 0");
+	cin >> userInput;
+	if (userInput == 1)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
 int main() {
 
 	int a, b, operationNumber, calculateValue, userInput;
@@ -61,26 +88,12 @@ int main() {
 		cin >> a;
 		logger.logDataWithNewLine("Enter Second Number");
 		cin >> b;
-		logger.logDataWithNewLine("Enter the Operation number need to perform");
-		logger.logDataWithNewLine("1. Addition");
-		logger.logDataWithNewLine("2. Substraction");
-		logger.logDataWithNewLine("3. Multiply");
-		logger.logDataWithNewLine("4. Division");
-		cin >> operationNumber;
-
+		operationNumber = getArithmeticEquation();
 		calculateValue = calculator(a, b, operationNumber);
 		cout << "Sum of " << a << " and " << b << " is :: " << calculateValue << endl;
-
-		logger.logDataWithNewLine("If you want to do the calculation again press 1 otherwise 0");
-		cin >> userInput;
-		if (userInput == 1)
-		{
-			nextProcess = true;
-		}
-		else {
-			nextProcess = false;
-		}
+		nextProcess = doYouWantToProcessAgain();	
 	}
-	
 	cin.get();
 }
+
+
